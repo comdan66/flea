5401d0cc81ac9291a8b1b713f5a686595c3a04a9
OA Wu <comdan66@gmail.com>
2015-04-20 15:16:10 +0800

    Relay footer, and set title.

--- application/cell/views/frame_cell/header/content.php
+++ application/cell/views/frame_cell/header/content.php
@@ -1,6 +1,11 @@
 <div id="header">
   <div>
-    <a class="active" href="space.html">空格</a>
-    <a href="string.html">字串</a>
+    <div class='l'>
+      <a class="active" href="space.html">空格</a>
+      <a href="string.html">字串</a>
+    </div> 
+    <div class='r'>
+      <a href="string.html">字串</a>
+    </div> 
   </div>
 </div>

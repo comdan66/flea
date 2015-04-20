aa9da3283e5a2fbbe02cea93d82386bc1a7b1d92
OA Wu <comdan66@gmail.com>
2015-04-20 16:08:50 +0800

    List layout demo.

--- application/core/controllers/03_site_controller.php
+++ application/core/controllers/03_site_controller.php
@@ -36,6 +36,8 @@ class Site_controller extends Oa_controller {
   private function _add_js () {
     return $this->add_js (base_url ('resource', 'javascript', 'jquery_v1.10.2', 'jquery-1.10.2.min.js'))
                 ->add_js (base_url ('resource', 'javascript', 'jquery-rails_d2015_03_09', 'jquery_ujs.js'))
+                ->add_js (base_url ('resource', 'javascript', 'jquery-timeago_v1.3.1', 'jquery.timeago.js'))
+                ->add_js (base_url ('resource', 'javascript', 'jquery-timeago_v1.3.1', 'locales', 'jquery.timeago.zh-TW.js'))
                 ;
   }
 }
--- application/views/content/site/main/index/content.php
+++ application/views/content/site/main/index/content.php
@@ -1,3 +1,30 @@
 <?php echo render_cell ('frame_cell', 'header', array ()); ?>
 
+  <div id='container'>
+    <table class='table-list'>
+      <thead>
+        <tr>
+          <th width='120'>新增日期</th>
+          <th width='120'>刊登者</th>
+          <th width='60'>買賣</th>
+          <th >標題</th>
+          <th width='100'>價格</th>
+          <th width='150'>類別</th>
+          <th width='100'>地區</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class='timeago' data-time='2013/10/10 10:10:10'>2013/10/10 10:10:10</td>
+          <td>OA</td>
+          <td>賣</td>
+          <td>asd</td>
+          <td>1200</td>
+          <td>iPhone 4s</td>
+          <td>臺北</td>
+        </tr>
+      <tbody>
+    </table>
+  </div>  
+  
 <?php echo render_cell ('frame_cell', 'footer', array ()); ?>
